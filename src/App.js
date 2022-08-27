bc5238e373f8c13ba65a659ef352083e8ac2b984
BroasaurBot <40313184+BroasaurBot@users.noreply.github.com>
2022-08-27 17:06:27 +1000

    trouble with states

--- src/App.js
+++ src/App.js
@@ -4,26 +4,43 @@ import Compare from './components/Compare';
 import EnterForm from './components/EnterForm';
 
 function App() {
-  const [options, setOptions] = useState(["Cheese", "Potato"]);
+  const [options, setOptions] = useState(["Cheese", "Potato", "Sasuage", "Bacon", "Lettuce"]);
   const [mode, changeMode] = useState("add");
+
   const [optionPair, compare] = useState(['', '']);
   const [rankedPair, rankPair] = useState(['', '']);
   
 
-  function addTask(newOp) {
+  function addOption(newOp) {
     let copy = [...options];
     copy = [...copy, newOp];
     setOptions(copy);
   }
 
+  function removeOption(Op) {
+    let copy = [];
+    for (let i = 0; i < options.length; i++) {
+      if(options[i] !== rankedPair[1]) {
+        copy.push(options[i]);
+      }
+
+    }
+    setOptions(copy);
+  }
+
   const beginComparing = () => {
+
+    if (options.length <= 1) {
+      changeMode("result");
+      return;
+    }
     compare([options[0], options[1]]);
-    rankPair(['', '']);
-    changeMode("compare");
   }
   
   const optionSelected = () => {
-    console.log("Selected");
+    console.log("Selected " , rankedPair[0]);
+    removeOption(rankedPair[1]);
+    beginComparing();
   }
 
   let displayed = <></>;
@@ -36,20 +53,30 @@ function App() {
           })}
       </div> 
 
-      <EnterForm addTask = {addTask} />
-      <button onClick={() => {beginComparing()}}>Ready to decide</button>
+      <EnterForm addTask = {addOption} />
+      <button onClick={() => {changeMode("compare"); beginComparing()}}>Ready to decide</button>
     </>
 
 
   } else if (mode === "compare") {
     displayed =
     <>
+      <div>
+        {options.map((option) => {
+          return <div>{option}</div>;
+          })}
+      </div> 
       <h3>Pick your favourite</h3>
       <Compare options={[optionPair[0], optionPair[1]]} rank={rankPair} selected={optionSelected}/>
-      <p>
-        {rankedPair[0]}
-      </p>
     </>
+
+  } else if (mode==="result") {
+    <p>
+      <h1>
+        Your decision is
+      </h1>
+      {options[0]}
+    </p>
   }
 
   return (
