13eecf923c513f9962d6867b380a4fa797291f72
your_username <yaoke259@outlook.com>
2022-08-27 16:29:35 +1000

    Fixed merges, added mode

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
 import './App.css';
 import React, { useState } from 'react';
 import Compare from './components/Compare';
-import EnterForm from '../src/EnterForm';
+import EnterForm from './components/EnterForm';
 
 function App() {
   const [options, setOptions] = useState(["Cheese", "Potato"]);
-  const [optionPair, compare] = useState(["", ""]);
+  const [mode, changeMode] = useState("add");
+  const [optionPair, compare] = useState(['', '']);
   const [rankedPair, rankPair] = useState(['', '']);
+  
 
   function addTask(newOp) {
     let copy = [...options];
     copy = [...copy, newOp];
     setOptions(copy);
   }
+
+  const beginComparing = () => {
+    compare([options[0], options[1]]);
+    rankPair(['', '']);
+    changeMode("compare");
+  }
   
   const optionSelected = () => {
     console.log("Selected");
+  }
+
+  let displayed = <></>;
+  if (mode === "add") {
+    displayed = 
+    <>
+      <div>
+        {options.map((option) => {
+          return <div>{option}</div>;
+          })}
+      </div> 
+
+      <EnterForm addTask = {addTask} />
+      <button onClick={() => {beginComparing()}}>Ready to decide</button>
+    </>
+
+
+  } else if (mode === "compare") {
+    displayed =
+    <>
+      <h3>Pick your favourite</h3>
+      <Compare options={[optionPair[0], optionPair[1]]} rank={rankPair} selected={optionSelected}/>
+      <p>
+        {rankedPair[0]}
+      </p>
+    </>
   }
 
   return (
@@ -27,13 +61,8 @@ function App() {
         </h1>
       </div>
 
-      <div>
-        {options.map((option) => {
-          return <div>{option}</div>;
-        })}
-      </div> 
-      <EnterForm addTask = {addTask} />
-      <Compare options={[optionPair[0], optionPair[1]]} rank={rankPair} selected={optionSelected}/>
+      {displayed}
+    
     </div>
   );
 }
